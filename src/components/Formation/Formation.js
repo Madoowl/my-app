457f73b7f135cc7cf7892bcac1a63f84089e6acd
Madowwl <emma.arlanda@viacesi.fr>
2023-01-23 11:02:08 +0100

    feat: add components
    add formation components

---
+++ src/components/Formation/Formation.js
@@ -0,0 +1,12 @@
+function Formation(){
+    return(
+        <div className="formation">
+            <h2 className="formation-title"> Formation title </h2>
+            <h4 className="formation-dates">from t to u</h4>
+            <h4 className="formation-place">place</h4>
+            <p className="formation-details">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+        </div>
+    )
+}
+
+export default Formation;
---
+++ src/components/Formation/FormationList.js
@@ -0,0 +1,15 @@
+import Formation from './Formation'
+
+function FormationList(){
+    return(
+        <>
+        <h1 className="formation-list">Formation List</h1>
+        <Formation />
+        <Formation />
+        <Formation />
+        <Formation />
+        </>
+    )
+}
+
+export default FormationList;
