68b359a7b200f6a873d7365def5b6a597f67f5b1
Madowwl <emma.arlanda@viacesi.fr>
2023-01-23 11:01:48 +0100

    feat: add components
    Add experience components

---
+++ src/components/Experience/Experience.js
@@ -0,0 +1,12 @@
+function Experience(){
+    return(
+        <div className="experience">
+            <h2 className="experience-title"> Experience title </h2>
+            <h4 className="experience-dates">from t to u</h4>
+            <h4 className="experience-place">place</h4>
+            <p className="experience-details">Lorem ipsum dolor sit amet, consectetur adipiscing elit - experience details</p>
+        </div>
+    )
+}
+ 
+export default Experience;
---
+++ src/components/Experience/ExperienceList.js
@@ -0,0 +1,17 @@
+import Experience
+ from "./Experience";
+
+ function ExperienceList(){
+    return(
+        <>
+        <h1 className="experience-list">Experience list</h1>
+        <Experience/>
+        <Experience/>
+        <Experience/>
+        <Experience/>
+        </>
+
+    )
+}
+
+export default ExperienceList;
